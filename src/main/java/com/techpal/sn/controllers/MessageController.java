6c12fbc9c41ec0c34d712da5307301f99e8f5118
Mendel <mendel@MBP-de-Mendel.lan>
2023-09-17 12:46:26 +0200

    Refract message mangement

--- src/main/java/com/techpal/sn/controllers/MessageController.java
+++ src/main/java/com/techpal/sn/controllers/MessageController.java
@@ -2,14 +2,12 @@ package com.techpal.sn.controllers;
 
 import com.techpal.sn.dto.MessageDTO;
 import com.techpal.sn.models.Messages;
-import com.techpal.sn.repository.MessageRepository;
 import com.techpal.sn.services.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.Timestamp;
-import java.time.Instant;
 import java.util.List;
 @CrossOrigin
 @RestController
@@ -18,19 +16,16 @@ public class MessageController {
 
     private final MessageService messageService;
 
-    private final MessageRepository messageRepository;
-
     @Autowired
-    public MessageController(MessageService messageService,MessageRepository messageRepository) {
+    public MessageController(MessageService messageService) {
         this.messageService = messageService;
-        this.messageRepository = messageRepository;
     }
 
     @CrossOrigin
     @GetMapping
     public ResponseEntity<List<Messages>> getAllMessages() {
         List<Messages> messages = messageService.getAllMessages();
-        return ResponseEntity.ok(messages);
+        return ResponseEntity.status(HttpStatus.OK).body(messages);
     }
     @CrossOrigin
     @GetMapping("/{id}")
@@ -39,40 +34,25 @@ public class MessageController {
         if (messages == null) {
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok(messages);
+        return ResponseEntity.status(HttpStatus.OK).body(messages);
     }
     @CrossOrigin
     @PostMapping
     public ResponseEntity<Messages> createMessage(@RequestBody MessageDTO messages) {
         Messages savedMessages = messageService.addMessage(messages);
-        return ResponseEntity.ok(savedMessages);
+        return ResponseEntity.status(HttpStatus.OK).body(savedMessages);
     }
     @CrossOrigin
     @PutMapping("/{id}")
-    public ResponseEntity<Messages> updateMessage(@PathVariable Long id, @RequestBody Messages messages) {
-        Messages existingMessages = messageService.getMessageById(id);
-        
-        if (existingMessages == null) {
-            return ResponseEntity.notFound().build();
-        }
-        
-        existingMessages.setMessage(messages.getMessage());
-        existingMessages.setUpdatedAt(Timestamp.from(Instant.now()));
-        
-        Messages updatedMessages = messageRepository.save(existingMessages);
-        return ResponseEntity.ok(updatedMessages);
+    public ResponseEntity<Messages> updateMessage(@PathVariable Long id, @RequestBody MessageDTO messagesDTO) {
+        Messages messages = messageService.updateMessage(id,messagesDTO);
+        return ResponseEntity.status(HttpStatus.OK).body(messages);
     }
     @CrossOrigin
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteMessage(@PathVariable Long id) {
-        Messages existingMessages = messageService.getMessageById(id);
-        
-        if (existingMessages == null) {
-            return ResponseEntity.notFound().build();
-        }
-        
-        messageRepository.deleteById(id);
-        return ResponseEntity.noContent().build();
+         messageService.deleteMessage(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
     @RequestMapping(value = "/",method = RequestMethod.OPTIONS)
     public void handleOptionsRequest() {
